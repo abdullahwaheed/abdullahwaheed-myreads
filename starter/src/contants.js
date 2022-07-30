dd2d4b0474f2409e53095cc0c1040c7dd6cbc265
Abdullah Waheed <abdullah.waheed@arbisoft.com>
2022-07-30 15:09:27 +0500

    feat: added book and category component

--- starter/src/contants.js
+++ starter/src/contants.js
@@ -1,4 +1,13 @@
+import PropTypes from 'prop-types';
+
 export const ROUTES = {
     HOME: '/',
     SEARCH: '/search',
+};
+
+export const BOOK_SHAPE = {
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
 };
