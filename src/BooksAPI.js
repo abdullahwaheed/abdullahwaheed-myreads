c7830ae91e487a0227ab9a32866536b97785c50d
Abdullah Waheed <abdullah.waheed@arbisoft.com>
2022-07-31 18:27:20 +0500

    refactor: removed default

--- src/BooksAPI.js
+++ src/BooksAPI.js
@@ -29,7 +29,7 @@ export const update = (book, shelf) =>
     body: JSON.stringify({ shelf }),
   }).then((res) => res.json());
 
-export const search = (query, maxResults=50) =>
+export const search = (query, maxResults) =>
   fetch(`${api}/search`, {
     method: "POST",
     headers: {
